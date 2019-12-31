7006dcb5f4134f43fd322180c0553bbafd5f48fd
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-12-31 10:18:03 -0800

    Updating repo URL

--- db/migrate/20191220052548_create_users.rb
+++ db/migrate/20191220052548_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :name
-      t.string :icon_gif
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2019_12_20_053034) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "name"
-    t.string "icon_gif"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
